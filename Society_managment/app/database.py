05d077a8f492b3f7ad9aa81cb48465b714069981
httpsDany <ahmaddanyal0504@gmail.com>
2025-06-29 12:33:32 +0530

    Update database.py

--- Society_managment/app/database.py
+++ Society_managment/app/database.py
@@ -13,7 +13,7 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 
-DATABASE_URL = f"mysql+mysqlconnector://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+DATABASE_URL = f"postgresql://society_management_sipu_user:JmEjQhJ9Fa0ZsiQ5P3m5mH6XYFxMLBAS@dpg-d1ge92umcj7s73clsn3g-a.oregon-postgres.render.com/society_management_sipu"
 
 from sqlalchemy.orm import Session
 from fastapi import Depends
