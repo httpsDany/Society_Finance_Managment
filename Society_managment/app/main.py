aab3419f394f64d8d6d70f6ca312f60d788bf697
httpsDany <ahmaddanyal0504@gmail.com>
2025-06-29 23:01:34 +0530

    Update main.py

--- Society_managment/app/main.py
+++ Society_managment/app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 from app.auth import auth, oauth
 from app.core import management,sync
+from app.database import Base, engine
+from app.models import flat_user
+
 
 app = FastAPI()
+
+# Automatically create tables in the database on startup (safe if tables already exist)
+Base.metadata.create_all(bind=engine)
 
 app.include_router(auth.router, prefix="/auth", tags=["Auth"])
 app.include_router(oauth.router, prefix="", tags=["OAuth"])
