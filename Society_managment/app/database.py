8deb60e47f3702fd9436bfc753dd2cedb230c555
httpsDany <ahmaddanyal0504@gmail.com>
2025-06-29 12:44:55 +0530

    Update database.py

--- Society_managment/app/database.py
+++ Society_managment/app/database.py
@@ -1,22 +1,14 @@
 # database.py
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
-from dotenv import load_dotenv
-import os
-
-load_dotenv()
-
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_HOST = os.getenv("DB_HOST")
-DB_PORT = os.getenv("DB_PORT")
-DB_NAME = os.getenv("DB_NAME")
-
-DATABASE_URL = f"postgresql://society_management_sipu_user:JmEjQhJ9Fa0ZsiQ5P3m5mH6XYFxMLBAS@dpg-d1ge92umcj7s73clsn3g-a.oregon-postgres.render.com/society_management_sipu"
-
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import declarative_base, sessionmaker, Session
 from fastapi import Depends
+
+# Directly paste the full URL here
+DATABASE_URL = "postgresql://society_management_sipu_user:JmEjQhJ9Fa0ZsiQ5P3m5mH6XYFxMLBAS@dpg-d1ge92umcj7s73clsn3g-a.oregon-postgres.render.com/society_management_sipu"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+Base = declarative_base()
 
 def get_db():
     db = SessionLocal()
@@ -24,8 +16,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
-Base = declarative_base()
